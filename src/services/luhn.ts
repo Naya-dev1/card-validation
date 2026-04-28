2556cb0155657341033345288c255812497a0846
Naya-dev1 <onyekwelibestephanie@gmail.com>
2026-04-28 05:17:27 +0100

    Add Luhn algorithm validation service

---
+++ src/services/luhn.ts
@@ -0,0 +1,22 @@
+export function luhnCheck(cardNumber: string): boolean {
+  const digits = cardNumber.replace(/\D/g, '')
+  
+  if (digits.length === 0) return false
+
+  let sum = 0
+  let shouldDouble = false
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let digit = parseInt(digits[i], 10)
+
+    if (shouldDouble) {
+      digit *= 2
+      if (digit > 9) digit -= 9
+    }
+
+    sum += digit
+    shouldDouble = !shouldDouble
+  }
+
+  return sum % 10 === 0
+}
